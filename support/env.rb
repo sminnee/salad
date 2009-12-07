a2b68477c7801fa7e98b69f88fa8f605d3fb9da8
Sam Minnee <sam@silverstripe.com>
2009-12-07 16:53:03 +1300

    Fixed slow start in Firefox

--- support/env.rb
+++ support/env.rb
@@ -11,24 +11,51 @@ else
     require 'firewatir'
 
     class FireWatir::Firefox
-      # fixes the timeout caused by firefox taking ages to
-      # load, preventing a connection from being made to JSSH
-      # Note that this can't be placed in firewatir-mods
-      def set_defaults(no_of_tries = 0)
+      # Modified implementation of Firewatir start-up to give it up to 30 seconds to get things sorted
+      def initialize(options = {})
+        if(options.kind_of?(Integer))
+          options = {:waitTime => options}
+        end
+
+        # check for jssh not running, firefox may be open but not with -jssh
+        # if its not open at all, regardless of the :suppress_launch_process option start it
+        # error if running without jssh, we don't want to kill their current window (mac only)
+        jssh_down = false
+        begin
+          set_defaults()
+        rescue Watir::Exception::UnableToStartJSShException
+          jssh_down = true
+        end
+
+        if current_os == :macosx && !%x{ps x | grep firefox-bin | grep -v grep}.empty?
+          raise "Firefox is running without -jssh" if jssh_down
+          open_window unless options[:suppress_launch_process]
+        elsif not options[:suppress_launch_process]
+          launch_browser(options)
+        end
+
+        set_defaults(300)
+        get_window_number()
+        set_browser_document()
+      end
+
+      def set_defaults(no_of_tries = 3)
+        no_of_tries_so_far = 0
+        # JSSH listens on port 9997. Create a new socket to connect to port 9997.
         begin
           $jssh_socket = TCPSocket::new(MACHINE_IP, "9997")
           $jssh_socket.sync = true
           read_socket()
         rescue
-          no_of_tries += 1
+          no_of_tries_so_far += 1
           sleep 0.1
-          retry if no_of_tries < 300
+          retry if no_of_tries_so_far < no_of_tries
           raise UnableToStartJSShException, "Unable to connect to machine : #{MACHINE_IP} on port 9997. Make sure that JSSh is properly installed and Firefox is running with '-jssh' option"
         end
         @error_checkers = []
       end
     end
-    
+
     Watir::Browser.default = 'firefox'
     Browser = Watir::Browser
     $killFF = true
@@ -57,10 +84,6 @@ end
 
 # Make it go fast - IE only
 # $browser.speed = :zippy
-
-# Reset database
-$browser.goto $baseURL + 'dev/tests/endsession'
-$browser.goto $baseURL + 'dev/tests/startsession?fixture=sapphire/tests/Bare.yml&flush=1'
 
 # Before each scenario, load in the globals
 Before do
