17ca4d68e0b6bace123b889ee3e7359db5800820
Luke Hudson <luke@192-168-1-188.local.silverstripe.com>
2010-03-29 18:08:24 +1300

    MINOR: Use  Salad.evaluate_script

--- step_definitions/browser/popups.rb
+++ step_definitions/browser/popups.rb
@@ -1,38 +1,14 @@
 Given /cancel pop-ups/i do
   script = "window.confirm=window.alert=window.prompt=function(){ return false; };"
-
-  # Special case for FireWatir
-  if @browser.respond_to?('evaluate_script_alternate')
-    @browser.evaluate_script_alternate(script)
-  elsif @browser.respond_to?('execute_script')
-    @browser.execute_script(script)
-  else
-    @browser.evaluate_script(script)
-  end
+	@salad.evaluate_script(script)
 end
 
 Given /confirm pop-ups/i do
   script ="window.confirm=window.alert=function(){ return true; }; window.prompt=function(){ return false; }";
-
-  # Special case for FireWatir
-  if @browser.respond_to?('evaluate_script_alternate')
-    @browser.evaluate_script_alternate(script)
-  elsif @browser.respond_to?('execute_script')
-    @browser.execute_script(script)
-  else
-    @browser.evaluate_script(script)
-  end
+	@salad.evaluate_script(script)
 end
 
 Given /put "(.*)" in the pop-up/i do | value |
   script = "window.confirm=window.alert=function(){ return true; }; window.prompt=function(){ return \"#{value}\"; }"
-
-  # Special case for FireWatir
-  if @browser.respond_to?('evaluate_script_alternate')
-    @browser.evaluate_script_alternate(script)
-  elsif @browser.respond_to?('execute_script')
-    @browser.execute_script(script)
-  else
-    @browser.evaluate_script(script)
-  end
+	@salad.evaluate_script(script)
 end
