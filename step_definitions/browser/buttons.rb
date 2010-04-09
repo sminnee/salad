e6fec3500f83ff85f2c012ebd70e2e548232b163
Luke Hudson <lukeletters@gmail.com>
2010-04-09 14:19:25 +1200

    BUGFIX: Fix XPath button location for Safari

--- step_definitions/browser/buttons.rb
+++ step_definitions/browser/buttons.rb
@@ -2,35 +2,45 @@
 # Does not matter what you select!
 # The proper watir code will be executed regardless.
 
-Given /click the "(.*)" button/i do |type|
-  btn = getButton(@salad.browser, type)
+Given /click the "(.*)" button/i do |which|
+  btn = getButton(@salad.browser, which)
   
   if(btn) then
     ajax_before_action @salad.browser
     btn.click
     ajax_after_action @salad.browser
   else
-    fail("could not find the '#{type}' button on #{@salad.browser.url}")
+    fail("could not find the '#{which}' button on #{@salad.browser.url}")
   end
 end
 
-def getButton(browser, type)
-  button = browser.button(:id, type)
-  button = browser.button(:value, type) unless button and button.exists? and button.visible?
+def getButton(browser, what)
+	field_elt = nil
+	hows = [:id, :name, :value, :index, :class]
 
-  # :text used for Firefox suport
-  if browser.instance_of?(FireWatir::Firefox)
-    button = browser.button(:text, type) unless button and button.exists? and button.visible?
-  end
+	hows.each {|how|
+		if how == :index and not what.is_a?(Numeric) then next end
+		field_elt = browser.button(how, what)
+		if field_elt and field_elt.exists? and field_elt.visible? then
+			return field_elt
+		end
+	}
+	# :text used for Firefox suport
+	if browser.instance_of?(FireWatir::Firefox)
+		field_elt = browser.button(:text, what)
+		return field_elt if field_elt and field_elt.exists? and field_elt.visible?
+	end
+	# Try to find control by XPath (safari support)
+	if not (field_elt and field_elt.exists? and field_elt.visible?) then
+		xpath = "//button[.=\"#{what}\"]"
+		field_elt = browser.button(:xpath, xpath)
+		return field_elt if field_elt and field_elt.exists? and field_elt.visible?
+	end
+	# Try to find control by its label
+	if not (field_elt and field_elt.exists? and field_elt.visible?) then
+		field_elt = @salad.byLabel(what) {|id| @salad.browser.button(:id, id)}
+	end
 
-  button = browser.button(:index, type) if type.is_a?(Numeric) and not (button and button.exists? and button.visible?)
-  button = browser.button(:class, type) unless button and button.exists? and button.visible?
-  button = browser.button(:name, type) unless button and button.exists? and button.visible?
-  
-  # :xpath used for Safari suport
-  button = browser.button(:xpath, "//button[.='#{type}']") unless button and button.exists? and button.visible?
-
-  
-  button = nil unless button and button.exists? and button.visible?
-  return button
+	field_elt = nil unless field_elt and field_elt.exists? and field_elt.visible?
+	return field_elt
 end
