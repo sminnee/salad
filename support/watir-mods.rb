e8ef4c4a550935c63c343f50ce0977fe5ded7d1a
Luke Hudson <lukeletters@gmail.com>
2010-06-30 14:38:30 +1200

    FEATURE: Add watir-mods for IE

---
+++ support/watir-mods.rb
@@ -0,0 +1,44 @@
+=begin
+
+This is a collection of modifications to SafariWatir to get it to work more consistently with the
+Watir API
+
+=end
+
+if $browserName and $browserName.downcase == 'ie'
+
+  require 'watir'
+
+  module Watir
+    class Frame
+    
+      def exists?
+        begin
+          locate if defined?(locate)
+        rescue WIN32OLERuntimeError
+          @o = nil
+        end
+        @o ? true: false
+      end # exists?
+
+      def visible?
+        object = @o
+        begin
+          if object.currentstyle.invoke('visibility') =~ /^hidden$/i
+            return false
+          end
+          if object.currentstyle.invoke('display') =~ /^none$/i
+            return false
+          end
+          if object.invoke('isDisabled')
+            return false
+          end
+        rescue WIN32OLERuntimeError
+        end
+        return true
+      end # visible?
+
+    end # class Frame
+  end # module Watir
+
+end # if $browserName ..
